adbd510a3d18acb02b20a602e036bf6b122651c5
irenlian <irynalian@stockx.com>
2021-11-14 23:24:59 +0200

    Calculate minimum waste

--- src/lib/__tests__/cnc.test.ts
+++ src/lib/__tests__/cnc.test.ts
@@ -10,4 +10,22 @@ describe('CNC', () => {
             expect(result[0].y).toEqual(0);
         });
     });
+
+    describe('minimum waste', () => {
+        it('calculate average waste', () => {
+            const res = Array.from(Array(100).keys(), key => {
+                const sheet = { w: Math.random() * 1000, l: Math.random() * 1000 };
+                const box = { w: Math.random() * 100, d: Math.random() * 100, h: Math.random() * 100 };
+                const sheetSquare = sheet.w * sheet.l;
+                const boxSquare = 2 * box.w * box.d + 2 * box.w * box.h + 2 * box.d * box.h;
+                const cnc = new CNC(sheet, box);
+                const boxes = cnc.combineBoxes().length;
+                return 1 - (boxSquare * boxes) / sheetSquare;
+            });
+
+            const s = res.reduce((sum, i) => sum + i, 0);
+
+            console.log(s / res.length);
+        });
+    });
 });
